28c4dbc2f1c53edd0264ec5be3716de8ed76101a
Ivan Ulanovich <ulanivan@yandex.ru>
2019-08-14 20:24:22 +0500

    fourth step ini

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,18 +2,10 @@ import _ from 'lodash';
 import path from 'path';
 import genDiff from '../src';
 
-test('genDiffJson', () => {
-  const before = path.resolve(__dirname, '__fixtures__', 'before.json');
-  const after = path.resolve(__dirname, '__fixtures__', 'after.json');
+test.each(['.json', '.yml', '.ini'])('genDiffTest', (e) => {
+  const before = path.resolve(__dirname, '__fixtures__', `before${e}`);
+  const after = path.resolve(__dirname, '__fixtures__', `after${e}`);
   const stringResult = 'host: hexlet.io\n + timeout: 20\n - timeout: 50\n - proxy: 123.234.53.22\n - follow: false\n + verbose: true';
   const expected = _.values({ stringResult }).join('');
-  expect(genDiff(before, after)).toEqual(expected);
-});
-
-test('genDiffYaml', () => {
-  const before = path.resolve(__dirname, '__fixtures__', 'before.yml');
-  const after = path.resolve(__dirname, '__fixtures__', 'after.yml');
-  const stringResult = 'host: hexlet.io\n + timeout: 40\n - timeout: 50\n + proxy: 123.234.44.55\n - proxy: 123.234.53.22\n - follow: false\n + verbose: false';
-  const expected = _.values({ stringResult }).join();
   expect(genDiff(before, after)).toEqual(expected);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const mapping = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default format => mapping[format];
